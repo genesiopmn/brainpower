49ee0996cae48afbe9b57e97a5737d6511f934af
Marcus Vinicius <marcus.silva@maps.com.br>
2021-11-03 20:57:28 -0300

    Entity TransactionHistory e JPA Transaction

---
+++ src/main/java/br/com/brainpower/bluebank/entity/TransactionHistory.java
@@ -0,0 +1,57 @@
+package br.com.brainpower.bluebank.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactionHistory")
+public class TransactionHistory extends SuperEntity{
+    
+    private String history;
+    private LocalDateTime date = LocalDateTime.now();
+    private BigDecimal value;
+    
+    @ManyToOne
+    @JoinColumn(name = "origin_accountId")
+    @JsonIgnore
+    private Account origin;
+    @ManyToOne
+    @JoinColumn(name = "destination_accountId")
+    @JsonIgnore
+    private Account destination;
+    
+    public TransactionHistory(){}
+
+    public TransactionHistory(String history, BigDecimal value, Account origin, Account destination) {
+        this.history = history;
+        this.value = value;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public String getHistory() {
+        return history;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public Account getOrigin() {
+        return origin;
+    }
+
+    public Account getDestination() {
+        return destination;
+    }
+}
